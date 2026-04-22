179949feca9a269f09c78661b1b0f595418b235b
BinhDP10 <daophucbinh041203@gmail.com>
2026-04-22 15:59:22 +0700

    vercel setting

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Tên repository của bạn trên GitHub
-  base: '/iHRP_Excel_Comparator/',
+  // Đặt là '/' để chạy đúng trên Vercel
+  base: '/',
   plugins: [react()],
   server: { port: 3000 }
 })
